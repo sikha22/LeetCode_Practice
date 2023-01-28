c1442dba18f7f00966106dc9b08651344439a4d0
sikha22 <84958102+sikha22@users.noreply.github.com>
2023-01-29 00:28:52 +0530

    Create MinStack.cpp

---
+++ Min Stack/MinStack.cpp
@@ -0,0 +1,35 @@
+class MinStack {
+public:
+    vector<int> st;
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push_back(val);
+    }
+    
+    void pop() {
+        if(st.size() == 0) return;
+        st.pop_back();
+    }
+    
+    int top() {
+        if(st.size() == 0) return -1;
+        return st.back();
+    }
+    
+    int getMin() {
+        if(st.size() == 0) return -1;
+        return *min_element(st.begin(), st.end());
+    }
+};
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
