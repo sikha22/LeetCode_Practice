efca80032ca585d303aec33bddd5c8f94ec2f6d1
sikha22 <84958102+sikha22@users.noreply.github.com>
2023-01-28 10:54:48 +0530

    Create pathSum.cpp

---
+++ Path Sum/pathSum.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root) return false;
+        if(root->left == NULL and root->right == NULL and targetSum - root->val == 0) return true;
+        return hasPathSum(root->left, targetSum-root->val) or hasPathSum(root->right, targetSum-root->val);
+    }
+};
