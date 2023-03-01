f6bf2b671271b4670baeb19b55cbf4cef03fc659
Sikha Kumari <84958102+sikha22@users.noreply.github.com>
2023-03-01 23:36:46 +0530

    Create Ransom Note.cpp

---
+++ Ransom Note/Ransom Note.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int counter[26] = {0};
+        for(char ch : magazine)
+            counter[ch - 'a']++;
+        for(char ch : ransomNote)
+            if(counter[ch - 'a']-- <= 0)
+                return false;
+        return true;  
+    }
+};
