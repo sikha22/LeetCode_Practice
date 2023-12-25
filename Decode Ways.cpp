b8a3860aff4f787581fd02469082a2f6ee7aee0b
Sikha Kumari <84958102+sikha22@users.noreply.github.com>
2023-12-25 15:01:56 +0530

    Update Decode Ways.cpp

--- Decode Ways.cpp
+++ Decode Ways.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    int numDecodingsRecursive(string s, int index) {
-        if (index == s.length()) {
-            return 1; // If we reach the end of the string, one way to decode it.
-        }
-
-        if (s[index] == '0') {
-            return 0; // If the current digit is '0', it cannot be decoded individually.
-        }
-
-        int ways = numDecodingsRecursive(s, index + 1); // Decode current digit individually
-
-        if (index + 1 < s.length() && (s[index] == '1' || (s[index] == '2' && s[index + 1] <= '6'))) {
-            ways += numDecodingsRecursive(s, index + 2); // Decode current and next digit together
-        }
-
-        return ways;
-    }
-
     int numDecodings(string s) {
-        return numDecodingsRecursive(s, 0);
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        dp[n] = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            if (s[i] != '0') dp[i] += dp[i+1];
+            if (i+1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6'))
+                dp[i] += dp[i+2];
+        }
+        return dp[0];
     }
 };
